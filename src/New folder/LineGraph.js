f9ca81b54186b132beea9f7c471ed8ceed6d5db8
unknown <shazaly.se@gmail.com>
2021-04-20 11:47:55 -0700

    B

---
+++ src/New folder/LineGraph.js
@@ -0,0 +1,106 @@
+import { FormatListNumberedRtlSharp } from '@material-ui/icons';
+import React, { useEffect, useState } from 'react'
+import {Line} from 'react-chartjs-2'
+import numeral from 'numeral'
+const options ={
+    legend: {
+        display:false
+    },
+    elements:{
+        point:{
+            radius:0
+        },
+    },
+    mainAspectRatio:false,
+    tooltips:{
+        mode: "index",
+        intersect:false,
+        callbacks:{
+            label:function(tooltipItem,data){
+                return numeral(tooltipItem.value).format("+0,0");
+            },
+        },
+    },
+    scales:{
+        xAxes: [
+            {
+                type: "time",
+                time:{
+                    format:"MM/DD/YY",
+                    tooltipFormat:"ll",
+                }
+            }
+        ],
+        yAxes:[
+            {
+                gridLines:{
+                    display:false
+                },
+                ticks:{
+                    callback:function(value,index,values){
+                        return numeral(value).format("0a");
+                    },
+                },
+            }
+        ]
+    }
+}
+const buildChartData = (data,casesType="cases") =>{
+    let chartData=[];
+    let lastDataPoint;
+   for(let date in data.cases){
+        if(lastDataPoint){
+            const newDataPoint={
+                x:date,
+                y:data[casesType][date] - lastDataPoint
+            };
+            chartData.push(newDataPoint)
+
+        }
+        lastDataPoint = data[casesType] [date];
+    }
+    return chartData;
+};
+function LineGraph({casesType ="cases"}) {
+    const [data,setData] = useState({});
+
+
+    useEffect(() =>{
+        const fetchData = async() =>{
+           await fetch("https://disease.sh/v3/covid-19/historical/all?lastday=120")
+        .then(response =>response.json())
+        .then(data =>{
+         let chartData =buildChartData(data,"cases")
+         console.log("chartData",data)
+         setData(chartData)
+         
+        });
+        }
+        fetchData();
+    },[casesType]);
+
+  
+    return (
+        <div>
+            <h1>Graph</h1>
+            {data?.length > 0&& (
+      <Line
+      options={options}
+      data={{
+          datasets: [
+           {   
+               backgroundColor:"rgba(204,16,52,0.5)",
+               borderColor:"#CC1034",
+               data: data
+              }
+          ],
+      }}
+      
+       />
+            )}
+      
+        </div>
+    )
+}
+
+export default LineGraph
---
+++ src/New folder/MapPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Map.css";
+
+
+function MapPage() {
+  return (
+    <div className="map">
+   sgfsgfgs
+    </div>
+  );
+}
+
+export default MapPage;
